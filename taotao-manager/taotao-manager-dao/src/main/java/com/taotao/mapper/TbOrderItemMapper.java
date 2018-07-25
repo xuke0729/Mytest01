e71cf58d31da54e9facf82ea345d1d142659a376
XUKE-PC <646278087@qq.com>
2018-07-25 21:19:28 +0800

    整合mybatis service dao

---
+++ taotao-manager/taotao-manager-dao/src/main/java/com/taotao/mapper/TbOrderItemMapper.java
@@ -0,0 +1,23 @@
+package com.taotao.mapper;
+
+import com.taotao.pojo.TbOrderItem;
+import com.taotao.pojo.TbOrderItemExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface TbOrderItemMapper {
+    int countByExample(TbOrderItemExample example);
+
+    int deleteByExample(TbOrderItemExample example);
+
+    int insert(TbOrderItem record);
+
+    int insertSelective(TbOrderItem record);
+
+    List<TbOrderItem> selectByExample(TbOrderItemExample example);
+
+    int updateByExampleSelective(@Param("record") TbOrderItem record, @Param("example") TbOrderItemExample example);
+
+    int updateByExample(@Param("record") TbOrderItem record, @Param("example") TbOrderItemExample example);
+}
---
+++ taotao-manager/taotao-manager-dao/src/main/java/com/taotao/mapper/TbOrderMapper.java
@@ -0,0 +1,23 @@
+package com.taotao.mapper;
+
+import com.taotao.pojo.TbOrder;
+import com.taotao.pojo.TbOrderExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface TbOrderMapper {
+    int countByExample(TbOrderExample example);
+
+    int deleteByExample(TbOrderExample example);
+
+    int insert(TbOrder record);
+
+    int insertSelective(TbOrder record);
+
+    List<TbOrder> selectByExample(TbOrderExample example);
+
+    int updateByExampleSelective(@Param("record") TbOrder record, @Param("example") TbOrderExample example);
+
+    int updateByExample(@Param("record") TbOrder record, @Param("example") TbOrderExample example);
+}
